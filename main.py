27bc342f9e8818b6cc9681c4efea500ebad0ed71
LeoMcLucky <47698500+LeoMcLucky@users.noreply.github.com>
2025-02-18 02:54:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,66 +7,117 @@ import os
 def main():
 
     cryptic_dict = {
-        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        'а': 'а͠',
+        'б': 'б̋',
+        'в': 'в͒͠',
+        'г': 'г͒͠',
+        'д': 'д̋',
+        'е': 'е͠',
+        'ё': 'ё͒͠',
+        'ж': 'ж͒',
+        'з': 'з̋̋͠',
+        'и': 'и',
+        'й': 'й͒͠',
+        'к': 'к̋̋',
+        'л': 'л̋͠',
+        'м': 'м͒͠',
+        'н': 'н͒',
+        'о': 'о̋',
+        'п': 'п̋͠',
+        'р': 'р̋͠',
+        'с': 'с͒',
+        'т': 'т͒',
+        'у': 'у͒͠',
+        'ф': 'ф̋̋͠',
+        'х': 'х͒͠',
+        'ц': 'ц̋',
+        'ч': 'ч̋͠',
+        'ш': 'ш͒͠',
+        'щ': 'щ̋',
+        'ъ': 'ъ̋͠',
+        'ы': 'ы̋͠',
+        'ь': 'ь̋',
+        'э': 'э͒͠͠',
+        'ю': 'ю̋͠',
+        'я': 'я̋',
+        'А': 'А͠',
+        'Б': 'Б̋',
+        'В': 'В͒͠',
+        'Г': 'Г͒͠',
+        'Д': 'Д̋',
+        'Е': 'Е',
+        'Ё': 'Ё͒͠',
+        'Ж': 'Ж͒',
+        'З': 'З̋̋͠',
+        'И': 'И',
+        'Й': 'Й͒͠',
+        'К': 'К̋̋',
+        'Л': 'Л̋͠',
+        'М': 'М͒͠',
+        'Н': 'Н͒',
+        'О': 'О̋',
+        'П': 'П̋͠',
+        'Р': 'Р̋͠',
+        'С': 'С͒',
+        'Т': 'Т͒',
+        'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠',
+        'Х': 'Х͒͠',
+        'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠',
+        'Ш': 'Ш͒͠',
+        'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠',
+        'Ы': 'Ы̋͠',
+        'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠',
+        'Ю': 'Ю̋͠',
+        'Я': 'Я̋',
         ' ': ' '
     }
 
     directory_name = "NPC"
     os.makedirs(directory_name, exist_ok=True)
+    directory_path = os.path.join(directory_name, 'form_{}.svg')
 
     skills = [
-        "Стремительный прыжок", "Электрический выстрел",
-        "Ледяной удар", "Стремительный удар", "Кислотный взгляд",
-        "Тайный побег", "Ледяной выстрел", "Огненный заряд"
+        "Стремительный прыжок",
+        "Электрический выстрел",
+        "Ледяной удар",
+        "Стремительный удар",
+        "Кислотный взгляд",
+        "Тайный побег",
+        "Ледяной выстрел",
+        "Огненный заряд"
     ]
 
     runic_skills = []
 
     for skill in skills:
         for letter in skill:
-            skill=skill.replace(letter, cryptic_dict[letter])
+            skill = skill.replace(letter, cryptic_dict[letter])
         runic_skills.append(skill)
 
     for number in range(1, 11):
         skills_list = random.sample(runic_skills, 3)
         fake = Faker('ru_RU')
         context = {
-      	 'first_name': fake.first_name(),
-      	 'last_name': fake.last_name(),
-      	 'job': fake.job(),
-      	 'town': fake.city(),
-      	 'strength': random.randint(3, 18),
-      	 'agility': random.randint(3, 18),
-      	 'endurance': random.randint(3, 18),
-      	 'intelligence': random.randint(3, 18),
-      	 'luck': random.randint(3, 18),
-      	 'skill_1': skills_list[0],
-      	 'skill_2': skills_list[1],
-      	 'skill_3': skills_list[2],
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+            'job': fake.job(),
+            'town': fake.city(),
+            'strength': random.randint(3, 18),
+            'agility': random.randint(3, 18),
+            'endurance': random.randint(3, 18),
+            'intelligence': random.randint(3, 18),
+            'luck': random.randint(3, 18),
+            'skill_1': skills_list[0],
+            'skill_2': skills_list[1],
+            'skill_3': skills_list[2],
         }
-        file_operations.render_template('charsheet.svg', \
-          r'NPC\form_{}.svg'.format(number), context)
+        file_operations.render_template(
+            'charsheet.svg', directory_path.format(number), context)
+
 
 if __name__ == '__main__':
-  main()
+    main()
